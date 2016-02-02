3c7092798aef16fbb304ca11cf246f8f7946c478
MissElfy <misselfy@gmail.com>
2016-02-02 15:59:23 +0100

    En cours
    Animation sans fonction

--- fonction_animee.py
+++ fonction_animee.py
@@ -2,6 +2,34 @@
 """
 Created on Tue Feb 02 15:19:40 2016
 
-@author: Ama
+@author: Ama 
 """
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+x = np.linspace(0.,10.,1000)
+y = np.sin(-x / 10.) * np.sin(2.* np.pi * x)
+
+
+fig = plt.figure("Animation !") 
+plt.clf()
+line, = plt.plot([], [])
+plt.xlim(0., 10.)
+plt.ylim(0., 10.)
+
+
+def init():
+    line.set_data([], [])
+    return line,
+    
+def animate(i):
+    line.set_data(x[:i], y[:i])
+    return line,
+    
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+frames=1000, interval=20, blit=True)
+
+plt.show()
+
 
