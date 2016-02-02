93bf51956542f03488dcd307ed18c3084d21edd9
Lorena <lorenafdutra@gmail.com>
2016-02-02 15:56:32 +0100

    Second commit

--- fonction_animee.py
+++ fonction_animee.py
@@ -2,6 +2,18 @@
 """
 Created on Tue Feb 02 15:19:40 2016
 
-@author: Ama
+@author: Amandine et Lorena
+
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+X = np.linspace(-np.pi, np.pi, 256,endpoint=True)
+C,S = np.cos(X), np.sin(X)
+
+plt.plot(X,C)
+plt.plot(X,S)
+
+plt.show()
 
